f20a6d7b658ad6c48d6504f097358e3547dc78fe
rgarces <renatogc1996@gamil.com>
2021-11-07 21:49:30 -0500

    minor change

--- AwtoReto/app/src/main/java/com/example/awtoreto/presentation/JokeFlags.kt
+++ AwtoReto/app/src/main/java/com/example/awtoreto/presentation/JokeFlags.kt
@@ -1,11 +1,11 @@
 package com.example.awtoreto.presentation
 
 enum class JokeFlags(val flag: String) {
-    RELIGIOUS("Religious"),
-    POLITICAL("Political"),
-    RACIST("Racist"),
-    NSFW("Nsfw"),
-    EXPLICIT("Explicit"),
-    SEXIST("Sexist"),
-    NONE("None"),
+    RELIGIOUS(flag = "Religious"),
+    POLITICAL(flag = "Political"),
+    RACIST(flag = "Racist"),
+    NSFW(flag = "Nsfw"),
+    EXPLICIT(flag = "Explicit"),
+    SEXIST(flag = "Sexist"),
+    NONE(flag = "None"),
 }
